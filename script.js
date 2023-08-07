2e94dbb29fa4c57e54127476d6a289379033250b
joypatel24 <72862865+JoyPatel24@users.noreply.github.com>
2023-08-07 14:53:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,3 +6,43 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
+// Play or Pause the video
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+// Update play/pause button
+function updatePlayButton() {
+  toggle.textContent = video.paused ? '►' : '❚ ❚';
+}
+
+// Update the progress bar
+function updateProgressBar() {
+  const progressPercentage = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${progressPercentage}%`;
+}
+
+// Skip video forward or backward
+function skipVideo() {
+  const skipTime = parseFloat(this.dataset.skip);
+  video.currentTime += skipTime;
+}
+
+// Update volume and playback speed
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+// Event listeners
+video.addEventListener('click', togglePlay);
+toggle.addEventListener('click', togglePlay);
+video.addEventListener('play', updatePlayButton);
+video.addEventListener('pause', updatePlayButton);
+video.addEventListener('timeupdate', updateProgressBar);
+skipButtons.forEach(button => button.addEventListener('click', skipVideo));
+ranges.forEach(range => range.addEventListener('input', handleRangeUpdate));
